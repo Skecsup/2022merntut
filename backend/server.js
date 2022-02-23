663098a8e2bc2594e8fe0b049cb79e8992d69265
Skecsup <kicsindy.julius@gmail.com>
2022-02-23 18:56:20 +0100

    Added frontend ui

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
