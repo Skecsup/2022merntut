ac8738e3f8289138fc11c74c88fddd24a2f158f5
Skecsup <kicsindy.julius@gmail.com>
2022-02-24 12:52:30 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ const colors = require('colors')
 const dotenv  = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const port = process.env.PORT
 
 connectDB()
 
 
 const app = express()
-const port = process.env.PORT || 5000
+
 
 
 app.use(express.json())
@@ -23,7 +24,8 @@ app.use('/api/users', require('./routes/userRoutes'))
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
 }
 else{
     app.get('/', (req, res) => res.send('Please set to production'))
