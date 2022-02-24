899255e915ccecc66e002a9e0eeaec58670c4ef9
Skecsup <kicsindy.julius@gmail.com>
2022-02-24 12:04:12 +0100

    final, time to deploy

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -41,14 +41,14 @@ const updateGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('goal not found')
     }
-    const user = await User.findById(req.user.id)
+    
     //check user
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
     //make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -67,14 +67,15 @@ const deleteGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('goal not found')
     }
-    const user = await User.findById(req.user.id)
+
+    
     //check user
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
     //make sure the logged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
